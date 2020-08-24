9cd2122f2286c53a260a72310d04dd07a7cfa638
davidheay <deha121212@outlook.es>
2020-08-24 17:47:30 -0500

    .map() varios componentes

--- src/components/Recommendations/Recommendations.js
+++ src/components/Recommendations/Recommendations.js
@@ -2,13 +2,23 @@ import React, { Component } from "react";
 import Recommendation from "./Recommendation/Recommendation"
 
 class Recomendations extends Component {
+    recomendations = [{
+        text: "Lávate las manos con frecuencia.Usa agua y jabón o un desinfectante de manos a base de alcohol.",
+        number: "1"
+    }, {
+        text: "Mantén una distancia de seguridad con personas que tosan o estornuden.",
+        number: "2"
+    }, {
+        text: "Utiliza mascarilla cuando no sea posible mantener el distanciamiento físico.",
+        number: "3"
+    }, {
+        text: "Cuando tosas o estornudes, cúbrete la nariz y la boca con el codo flexionado o con un pañuelo.",
+        number: "4"
+    }];
     render() {
         return (
             <div className="row">
-                <Recommendation text="Lávate las manos con frecuencia. Usa agua y jabón o un desinfectante de manos a base de alcohol." number="1" />
-                <Recommendation text="Mantén una distancia de seguridad con personas que tosan o estornuden." number="2" />
-                <Recommendation text="Utiliza mascarilla cuando no sea posible mantener el distanciamiento físico." number="3" />
-                <Recommendation text="Cuando tosas o estornudes, cúbrete la nariz y la boca con el codo flexionado o con un pañuelo." number="4" />
+                {this.recomendations.map(item => (<Recommendation {...item} />))}
             </div>
         )
     }
--- src/components/Specialties/Specialties.js
+++ src/components/Specialties/Specialties.js
@@ -4,15 +4,26 @@ import psicologia from "../../images/psicologia.jpg";
 import cardiologia from "../../images/cardio.png";
 import fisioterapia from "../../images/fisioterapia.jpg";
 class Specialties extends Component {
+    specialties = [{
+        title: "Psicologia",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus purus id eros condimentum dignissim. Cras porta tortor id risus vestibulum rhoncus. ",
+        src: psicologia
+    }, {
+        title: "Cardiologia",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus purus id eros condimentum dignissim. Cras porta tortor id risus vestibulum rhoncus. ",
+        src: cardiologia
+    }, {
+        title: "Fisioterapia",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus purus id eros condimentum dignissim. Cras porta tortor id risus vestibulum rhoncus. ",
+        src: fisioterapia
+    }]
     render() {
         return (
             <div className="row">
                 <div className="col-12 mb-4">
                     <h2>Nuestras especialidades</h2>
                 </div>
-                <Specialty src={psicologia} title="Psicologia" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus purus id eros condimentum dignissim. Cras porta tortor id risus vestibulum rhoncus. " />
-                <Specialty src={cardiologia} title="Cardiologia" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus purus id eros condimentum dignissim. Cras porta tortor id risus vestibulum rhoncus. " />
-                <Specialty src={fisioterapia} title="Fisioterapia" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus purus id eros condimentum dignissim. Cras porta tortor id risus vestibulum rhoncus. " />
+                {this.specialties.map(item => (<Specialty  {...item} />))}
             </div>
         )
     }
