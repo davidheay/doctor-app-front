e769df50d2dc17c5ad995cdbd6ab8b3ca07cfb41
projectdesing <projectdesing20181@gmail.com>
2020-11-09 13:45:26 -0500

    Nosotros

--- src/pages/Us/Us.js
+++ src/pages/Us/Us.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import ilustration from '../../images/icons/clinica.png';
+import Specialties from '../../components/Specialties/Specialties';
 
 class Us extends Component {
   render() {
-    return (<></>
+    return (
+      <div class="container">
+        
+        
+        <div className="row info-section">
+          <div className="col-sm-6">
+            <div className="info-content">
+              <h2>Nos apasiona tu cuidado</h2>
+              <p>
+                Nos apasiona ayudarte a cuidarte, te cuidamos desde casa y vamos hasta tu casa, no te preocupes por nada, nos encargamos de todo
+              </p>
+              <Link className="btn btn-primary" to="/citas">
+                Reserva ahora
+              </Link>
+            </div>
+          </div>
+          <div className="col-sm-6 ">
+            <img src={ilustration} alt="Ilustration home" className="img-fluid"/>
+          </div>
+        </div>
+        <div className="mt-5">
+          <Specialties />
+        </div>
+     </div>
     )
   }
 }
